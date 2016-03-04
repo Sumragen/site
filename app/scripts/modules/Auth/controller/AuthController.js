3e8ba43b6fdc7e6bc5cd72cb9af876992fb98bce
Rostyslav Kurylo <rostislav.k@hotmail.com>
2016-03-04 16:41:32 +0200

    auth (signIn + signUp)

--- app/scripts/modules/Auth/controller/AuthController.js
+++ app/scripts/modules/Auth/controller/AuthController.js
@@ -2,25 +2,39 @@
  * Created by sumragen on 2/27/16.
  */
 define(['../module'], function (module) {
-    module.controller('AuthController', ['$scope', 'close', '$http', 'AuthService' ,function($scope, close, $http, AuthService) {
+    module.controller('AuthController', ['$scope', 'close', '$http', 'AuthService', function ($scope, close, $http, AuthService) {
         var self = this;
 
         self.close = close;
 
 
-        self.update = function() {
-            $http.get('/users').success(function(data) {
+        self.update = function () {
+            $http.get('/users').success(function (data) {
                 $scope.users = data;
             });
         };
-        self.addUser = function(newUser,newLogin,newPsw){
+        self.addUser = function (newUser, newLogin, newPsw) {
             console.log('add user');
-            $http.post('/users', {'name': newUser,'login':newLogin,'password':newPsw}).success(function() {
+            $http.post('/users', {'name': newUser, 'login': newLogin, 'password': newPsw}).success(function () {
                 self.update();
             });
         };
 
-        self.signIn = function (currentLogin,currentPassword) {
+        self.signIn = function (currentLogin, currentPassword) {
+            console.log('signIn');
+            $http.get('/users').success(function (data) {
+                $scope.users = data;
+                var response = {};
+                for (i = 0; i < $scope.users.length; i++) {
+                    response = {success: currentLogin === $scope.users[i].login && currentPassword === $scope.users[i].password};
+                    if (!response.success) {
+                        $scope.error = "Username or password is incorrect";
+                    } else {
+                        self.close();
+                        break;
+                    }
+                }
+            });
 
         };
         self.update();

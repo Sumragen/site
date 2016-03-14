e70dd0af07e1a45a9482804843848cdd3fed1fb9
Rostyslav Kurylo <rostislav.k@hotmail.com>
2016-03-14 17:45:24 +0200

    fix modalService

--- app/scripts/modules/Schedule/controller/ScheduleController.js
+++ app/scripts/modules/Schedule/controller/ScheduleController.js
@@ -2,22 +2,30 @@
  * Created by sumragen on 2/27/16.
  */
 define(['../module'], function (module) {
-    module.controller('ScheduleController', ['$scope', function ($scope) {
+    module.controller('ScheduleController', ['$scope', 'ModalService', function ($scope, ModalService) {
         var self = this;
         $scope.eventSources = [];
         $scope.uiConfig = {
-            calendar:{
+            calendar: {
                 height: 450,
                 editable: true,
-                header:{
+                header: {
                     left: 'month basicWeek basicDay agendaWeek agendaDay',
                     center: 'title',
                     right: 'today prev,next'
                 },
                 dayClick: function () {
                     console.log('click');
+                    self.showDayModal();
                 }
             }
         };
+        self.showDayModal = function () {
+            ModalService.showModal({
+                templateUrl: "./views/tabs/schedule/day.html",
+                controller: "AuthController as controller"
+            });
+
+        }
     }]);
 });

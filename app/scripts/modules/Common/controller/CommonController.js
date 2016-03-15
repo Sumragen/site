537c312c192500f5d2a5ea6cd04e4da4f4b5a6a7
Sumragen <Sumragen@gmail.com>
2016-03-15 21:12:57 +0200

    header

--- app/scripts/modules/Common/controller/CommonController.js
+++ app/scripts/modules/Common/controller/CommonController.js
@@ -5,9 +5,13 @@ define(['../module'], function (module) {
     module.controller('CommonController', ['$scope', 'ModalService', '$http', 'AuthService', '$state', function ($scope, ModalService, $http, AuthService, $state) {
         var self = this;
         $scope.state = $state;
-        self.getName = function () {
+        self.getUserName = function () {
             var user = JSON.parse(localStorage.getItem("currentUserLS"));
             return user.login;
+        };
+        self.getSchoolName = function () {
+            var user = JSON.parse(localStorage.getItem("currentUserLS"));
+            return 'School ' + '24';//+ user.school;
         };
         self.isAuthenticated = function () {
             return (localStorage.getItem("currentUserLS"));
@@ -35,6 +39,7 @@ define(['../module'], function (module) {
                 });
             });
         };
+
 
         self.logOut = function () {
             localStorage.removeItem("currentUserLS");

73756efd35c8b53a548407fab528c394bd4dc98c
Sumragen <Sumragen@gmail.com>
2016-03-23 13:55:29 +0200

    valid (temp)

--- app/scripts/modules/Dashboard/Profile/controller/ProfileController.js
+++ app/scripts/modules/Dashboard/Profile/controller/ProfileController.js
@@ -39,10 +39,30 @@ define(['../module'], function (module) {
             $scope.schema = {
                 type: "object",
                 properties: {
-                    firstName: {type: "string", minLength: 2, title: "First Name", description: "Your first name"},
-                    lastName: {type: "string", minLength: 2, title: "Last Name", description: "Your last name"},
-                    login: {type: "string", minLength: 4, title: "Login", description: "Your login"},
-                    email: {type: "string", minLength: 4, title: "Email", description: "Your email"}
+                    firstName: {
+                        type: "string",
+                        minLength: 2,
+                        title: "First Name",
+                        description: "Your first name",
+                        validationMessage: {
+                            'firstNameValid' : "Please, enter current first name"
+                        }
+                    },
+                    lastName: {
+                        type: "string",
+                        minLength: 2,
+                        title: "Last Name",
+                        description: "Your last name"},
+                    login: {
+                        type: "string",
+                        minLength: 4,
+                        title: "Login",
+                        description: "Your login"},
+                    email: {
+                        type: "string",
+                        minLength: 4,
+                        title: "Email",
+                        description: "Your email"},
                 }
             };
 

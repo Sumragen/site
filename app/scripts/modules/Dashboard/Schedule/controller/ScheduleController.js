0b3432db1d7b218b8e592e997cbdeb08cfe15182
Rostyslav Kurylo <rostislav.k@hotmail.com>
2016-03-24 17:24:22 +0200

    schedule calendar (fix)

--- app/scripts/modules/Dashboard/Schedule/controller/ScheduleController.js
+++ app/scripts/modules/Dashboard/Schedule/controller/ScheduleController.js
@@ -12,10 +12,6 @@ define(['../module'], function (module) {
         //'Common.ModalService',
         function ($scope, $rootScope, moment, $filter, $uibModal, scheduleService) {
             var self = this;
-            scheduleService.loadSchedule().then(function (data) {
-                $scope.currentSchedule = data.data.schedule;
-                createEventList($scope.currentSchedule.objects.schedule);
-            });
             self.showDayModal = function (date) {
                 var modalInstance = $uibModal.open({
                     animation: true,
@@ -55,11 +51,14 @@ define(['../module'], function (module) {
                 var s = new Date(start).getTime() / 1000;
                 var e = new Date(end).getTime() / 1000;
                 var m = new Date(start).getMonth();
-                var events = [];
-                callback(events);
+                scheduleService.loadSchedule().then(function (data) {
+                    $scope.currentSchedule = data.data.schedule;
+                    var events = createEventList($scope.currentSchedule.objects.schedule, start, end);
+                    callback(events);
+                });
             };
 
-            $scope.createEvent = function (lesson,step) {
+            $scope.createEvent = function (lesson,step, start, end) {
                 var startH = 8;
                 var startM = 30;
                 var endH = 9;
@@ -83,47 +82,50 @@ define(['../module'], function (module) {
                             endM = 50;
                     }
                 }
+                var d = new Date(start).getDate();
+                var m = new Date(start).getMonth();
+                var y = new Date(start).getFullYear();
 
-                var currentDayOfWeek = $filter('date')(moment(new Date()).format('dddd'), 'dddd');
-                var numDay = 0;
-                if( currentDayOfWeek === 'Monday'){
-                    numDay = 1;
-                }else{
-                    if( currentDayOfWeek === 'Tuesday'){
-                        numDay = 2;
-                    }else{
-                        if( currentDayOfWeek === 'Wednesday'){
-                            numDay = 3;
-                        }else{
-                            if( currentDayOfWeek === 'Thursday'){
-                                numDay = 4;
-                            }else{
-                                if( currentDayOfWeek === 'Friday'){
-                                    numDay = 5;
-                                }
-                            }
-                        }
-                    }
-                }
+                //var currentDayOfWeek = $filter('date')(moment(start).format('dddd'), 'dddd');
+                //var numDay = 0;
+                //if( currentDayOfWeek === 'Monday'){
+                //    step = 1;
+                //}else{
+                //    if( currentDayOfWeek === 'Tuesday'){
+                //        numDay = 2;
+                //    }else{
+                //        if( currentDayOfWeek === 'Wednesday'){
+                //            numDay = 3;
+                //        }else{
+                //            if( currentDayOfWeek === 'Thursday'){
+                //                numDay = 4;
+                //            }else{
+                //                if( currentDayOfWeek === 'Friday'){
+                //                    numDay = 5;
+                //                }
+                //            }
+                //        }
+                //    }
+                //}
 
-                var h = numDay - step;
+                var h = step;
 
                 $scope.events.push({
                     title: lesson.lesson,
-                    start: new Date(y, m, d - h, startH, startM),
-                    end: new Date(y, m, d - h, endH, endM),
+                    start: new Date(y, m, d + h, startH, startM),
+                    end: new Date(y, m, d + h, endH, endM),
                     allDay: false
                 });
             };
 
-            function createEventList(schedule) {
-                var step = 0;
+            function createEventList(schedule, start, end) {
+                var step = -1;
                 schedule.forEach(function (day) {
                     step++;
                     self.tempSchedule = scheduleService.parseLessons(day);
                     self.tempSchedule.forEach(function (lesson) {
                         if (lesson) {
-                            $scope.createEvent(lesson,step);
+                            $scope.createEvent(lesson,step, start,end);
                         }
                     });
                 });

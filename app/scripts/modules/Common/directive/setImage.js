8708119042023be58e448ff4d27e9d945918b7fa
Rostyslav Kurylo <rostislav.k@hotmail.com>
2016-03-30 12:47:06 +0300

    (fix) attribute syntax in profile.html

---
+++ app/scripts/modules/Common/directive/setImage.js
@@ -0,0 +1,15 @@
+/**
+ * Created by trainee on 3/30/16.
+ */
+define(['../module'], function (module) {
+    module.directive('sSetImage', [
+        function () {
+            return {
+                restrict: 'A',
+                link: function ($scope, element, attrs) {
+
+                }
+            }
+        }
+    ]);
+});

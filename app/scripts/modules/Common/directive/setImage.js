194ea7e2cc1470a301954162858ce20d0c2def04
Rostyslav Kurylo <rostislav.k@hotmail.com>
2016-03-30 19:21:16 +0300

    temp commit

--- app/scripts/modules/Common/directive/setImage.js
+++ app/scripts/modules/Common/directive/setImage.js
@@ -14,34 +14,28 @@ define(['../module'], function (module) {
                 compile: function compile(element, attrs) {
                     return {
                         pre: function (scope, element, attrs, ngModelController) {
-
-                        },
-                        post: function (scope, element, attrs, ngModelController) {
-
                             var image = new Image();
                             image.onload = function () {
                                 console.log('onload');
                                 element.css({
                                     backgroundImage: 'url(' + scope.imageUrl + ')',
                                     backgroundSize: 'cover',
-                                    width: 'auto',
+                                    width: '300px',
                                     height: '350px'
-                                })
+                                });
                             };
                             image.onerror = function () {
                                 console.log('onerror');
-                                element.css({
-                                    backgroundImage: 'url(images/' + scope.typeImage + '-not-found.png)',
-                                    width: 'auto',
-                                    height: '100px'
-                                });
+                                element.addClass('image-not-found');
                             };
                             image.src = scope.imageUrl;
+                        },
+                        post: function (scope, element, attrs, ngModelController) {
+
                         }
                     }
                 },
                 link: function (scope, element, attrs) {
-
 
                 },
                 //templateUrl: '/views/Common/setImage.html'

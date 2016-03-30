becbe5ede6c6176b02c08de10453560f9f90af05
Rostyslav Kurylo <rostislav.k@hotmail.com>
2016-03-30 17:55:03 +0300

    setImage directive

--- app/scripts/modules/Common/directive/setImage.js
+++ app/scripts/modules/Common/directive/setImage.js
@@ -10,7 +10,41 @@ define(['../module'], function (module) {
                     typeImage: '@sSetImage',
                     imageUrl: '='
                 },
-                templateUrl: '/views/Common/setImage.html'
+                require: '^ngModel',
+                compile: function compile(element, attrs) {
+                    return {
+                        pre: function (scope, element, attrs, ngModelController) {
+
+                        },
+                        post: function (scope, element, attrs, ngModelController) {
+
+                            var image = new Image();
+                            image.onload = function () {
+                                console.log('onload');
+                                element.css({
+                                    backgroundImage: 'url(' + scope.imageUrl + ')',
+                                    backgroundSize: 'cover',
+                                    width: 'auto',
+                                    height: '350px'
+                                })
+                            };
+                            image.onerror = function () {
+                                console.log('onerror');
+                                element.css({
+                                    backgroundImage: 'url(images/' + scope.typeImage + '-not-found.png)',
+                                    width: 'auto',
+                                    height: '100px'
+                                });
+                            };
+                            image.src = scope.imageUrl;
+                        }
+                    }
+                },
+                link: function (scope, element, attrs) {
+
+
+                },
+                //templateUrl: '/views/Common/setImage.html'
             }
         }
     ]);

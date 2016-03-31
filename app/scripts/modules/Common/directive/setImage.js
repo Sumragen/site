85aa2c3eba62a443eebd82d0d6bc1b4dfd70a513
Rostyslav Kurylo <rostislav.k@hotmail.com>
2016-03-31 13:58:24 +0300

    setImage directive ref

--- app/scripts/modules/Common/directive/setImage.js
+++ app/scripts/modules/Common/directive/setImage.js
@@ -6,31 +6,27 @@ define(['../module'], function (module) {
         function () {
             return {
                 restrict: 'A',
-                compile: function compile(element, attrs) {
-                    return {
-                        post: function (scope, element, attrs) {
-                            var errorClass = attrs['sClassOnError'];
-                            var imageUrl = attrs['imageUrl'];
+                link: function (scope, element, attrs){
+                    var errorClass = attrs['sClassOnError'];
+                    var imageUrl = attrs['imageUrl'];
 
-                            var image = new Image();
+                    var image = new Image();
 
-                            attrs.$observe('imageUrl', function(newUrl){
-                                element.css({
-                                    backgroundImage: 'url(' + newUrl + ')'
-                                });
-                            });
-                            image.onload = function () {
-                                element.addClass('avatar');
-                                element.css({
-                                    backgroundImage: 'url(' + imageUrl + ')'
-                                });
-                            };
-                            image.onerror = function () {
-                                element.addClass(errorClass || '');
-                            };
-                            image.src = imageUrl;
-                        }
-                    }
+                    attrs.$observe('imageUrl', function(newUrl){
+                        element.css({
+                            backgroundImage: 'url(' + newUrl + ')'
+                        });
+                    });
+                    image.onload = function () {
+                        element.addClass('avatar');
+                        element.css({
+                            backgroundImage: 'url(' + imageUrl + ')'
+                        });
+                    };
+                    image.onerror = function () {
+                        element.addClass(errorClass || '');
+                    };
+                    image.src = imageUrl;
                 }
             }
         }
